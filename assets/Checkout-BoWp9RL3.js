f87df145cbad46c0a4368ba0dc73b24aa248ca1b
Umang Rathour <rathourumang@gmail.com>
2025-02-01 18:55:24 +0530

    Updates

--- assets/Checkout-BoWp9RL3.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BY1yb6Y6.js";const r=()=>t.jsx("div",{className:"flex justify-center items-center text-5xl",children:"Under Construction!!!"});export{r as default};
---
+++ assets/Checkout-DpcDRarm.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BlKT3PBV.js";const r=()=>t.jsx("div",{className:"flex justify-center items-center text-5xl",children:"Under Construction!!!"});export{r as default};
